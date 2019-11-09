c02b8be34ded85988fd5aeb3c9b4f2a5ff95c2be
Linxuan Xu <linxuanseph@gmail.com>
2019-11-09 10:19:23 -0600

    delete email

--- admin/notification.py
+++
@@ -1,5 +0,0 @@
-from flask_restful import reqparse, abort, Resource
-
-class Notification(Resource):
-	def send_email(self):
-		return "sss"
--- admin/server.py
+++ admin/server.py
@@ -4,7 +4,6 @@ from flagged_item import FlaggedItemList, FlaggedItem
 from category import CategoryList, Category
 from auction import AuctionList, Auction
 from user import UserList, User
-from notification import Notification
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,7 +16,6 @@ api.add_resource(AuctionList, '/auction')
 api.add_resource(Auction, '/auction/<auction_id>')
 api.add_resource(UserList, '/user')
 api.add_resource(User, '/user/<user_id>')
-api.add_resource(Notification, '/notification')
 
 if __name__ == '__main__':
     app.run(debug=True)
